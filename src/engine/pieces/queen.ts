2b8dc0f015588d7d2b2b2d0bcdb1f2481cf833b8
Darian Balagiu <Darian.Balagiu@softwire.com>
2024-07-03 13:41:03 +0300

    queen can move

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,13 +1,64 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        let moves: Square[] = [];
+        let square = board.findPiece(this)
+
+        // Rook stuff
+        for (let i = 0; i <= 7; i++) {
+            if (i != square.col) {
+                moves.push(Square.at(square.row, i))
+            }
+            if (i != square.row) {
+                moves.push(Square.at(i, square.col))
+            }
+        }
+
+        // Bishop stuff
+        let row = square.row + 1
+        let col = square.col + 1
+
+        while (row <= 7 && col <= 7) {
+            moves.push(Square.at(row, col))
+            row++
+            col++
+        }
+
+        row = square.row - 1
+        col = square.col - 1
+
+        while (row >= 0 && col >= 0) {
+            moves.push(Square.at(row, col))
+            row--
+            col--
+        }
+
+        row = square.row - 1
+        col = square.col + 1
+
+        while (row >= 0 && col <= 7) {
+            moves.push(Square.at(row, col))
+            row--
+            col++
+        }
+
+        row = square.row + 1
+        col = square.col - 1
+
+        while (row <= 7 && col >= 0) {
+            moves.push(Square.at(row, col))
+            row++
+            col--
+        }
+
+        return moves;
     }
 }
