3e8fba8b704a29a4da67dcd4e6e4dc793e8fca5d
Darian Balagiu <Darian.Balagiu@softwire.com>
2024-07-03 13:28:14 +0300

    rooks can move anywhere horizontally and vertically

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,13 +1,26 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        let moves: Square[] = [];
+        let square = board.findPiece(this)
+
+        for (let i = 0; i <= 7; i++) {
+            if (i != square.col) {
+                moves.push(Square.at(square.row, i))
+            }
+            if (i != square.row) {
+                moves.push(Square.at(i, square.col))
+            }
+        }
+
+        return moves;
     }
 }
