fa997a31177f7238f2fa411098fcc243c6ded391
Darian Balagiu <Darian.Balagiu@softwire.com>
2024-07-03 13:39:18 +0300

    bishops can move

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,13 +1,53 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        let moves: Square[] = [];
+        let square = board.findPiece(this)
+
+        let row = square.row + 1
+        let col = square.col + 1
+
+        while (row <= 7 && col <= 7) {
+            moves.push(Square.at(row, col))
+            row++
+            col++
+        }
+
+        row = square.row - 1
+        col = square.col - 1
+
+        while (row >= 0 && col >= 0) {
+            moves.push(Square.at(row, col))
+            row--
+            col--
+        }
+
+        row = square.row - 1
+        col = square.col + 1
+
+        while (row >= 0 && col <= 7) {
+            moves.push(Square.at(row, col))
+            row--
+            col++
+        }
+
+        row = square.row + 1
+        col = square.col - 1
+
+        while (row <= 7 && col >= 0) {
+            moves.push(Square.at(row, col))
+            row++
+            col--
+        }
+
+        return moves;
     }
 }
